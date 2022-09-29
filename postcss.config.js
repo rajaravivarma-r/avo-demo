b834451ade3a8bb7abe6aefde30e891741c29829
RajaRaviVarma <rajaravivarma.ar@gmail.com>
2022-09-29 17:35:03 +0530

    Add files to simulate the original app

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+let environment = {
   plugins: [
     require('postcss-import'),
+    require('tailwindcss')('tailwind.config.js'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
@@ -10,3 +11,20 @@ module.exports = {
     })
   ]
 }
+
+if (process.env.RAILS_ENV === "production" || process.env.RAILS_ENV === "staging") {
+  environment.plugins.push(
+      require('@fullhuman/postcss-purgecss')({
+        content: [
+          './app/**/*.html.erb',
+          './app/helpers/**/*.rb',
+          './app/javascript/**/*.js',
+          './app/javascript/**/*.jsx',
+          './node_modules/react-table-6/*.js',
+        ],
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      })
+  )
+}
+
+module.exports = environment
